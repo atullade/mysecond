365bd11f39236bd575faa31182f90a43420d56a0
atullade <guravatul1132@gmail.com>
2023-04-10 10:38:30 +0530

    First Commit

---
+++ src/main/java/git/gitHub11/App.java
@@ -0,0 +1,13 @@
+package git.gitHub11;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
