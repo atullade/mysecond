af0702c4a8a53e78fdf68c378777bb53c470321c
atullade <guravatul1132@gmail.com>
2023-04-10 11:03:47 +0530

    second commit

---
+++ src/test/java/git/gitHub11/firstlogin.java
@@ -0,0 +1,12 @@
+package git.gitHub11;
+
+public class firstlogin {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Welcome Java");
+
+	}
+
+}
